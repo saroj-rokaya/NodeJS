997236fb5f2cb296f324db848483051d2bfe3f3b
Saroj Rokaya <rokayasaroj188@gmail.com>
2024-08-22 23:35:45 +0545

    usermodel update

--- model/index.js
+++ model/index.js
@@ -1,41 +1,41 @@
 const { Sequelize, DataTypes } = require("sequelize");
-const databaseConfig = require("../config/dbConfig"); 
+const databaseConfig = require("../config/dbConfig");
 const makeBlogTable = require("./blogModel");
-
+const makeuserTable = require("./userModel.js");
 
 const sequelize = new Sequelize(
   databaseConfig.db,
   databaseConfig.username,
   databaseConfig.password,
   {
-    host: databaseConfig.host, 
-    port: databaseConfig.port, 
-    dialect: databaseConfig.dialect, 
+    host: databaseConfig.host,
+    port: databaseConfig.port,
+    dialect: databaseConfig.dialect,
     operatorsAliases: false,
 
     pool: {
-      max: 5, 
-      min: 0, 
-      acquire: 30000, 
-      idle: 10000, 
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
     },
   }
 );
 sequelize
-  .authenticate() 
+  .authenticate()
   .then(() => {
     console.log("match");
   })
   .catch((err) => {
-    console.log("error", err); 
+    console.log("error", err);
   });
 
 const db = {};
 db.Sequelize = Sequelize; //class
 db.sequelize = sequelize;
 
-
-db.blogs = makeBlogTable(sequelize,DataTypes);
+db.blogs = makeBlogTable(sequelize, DataTypes);
+db.users = makeuserTable(sequelize, DataTypes);
 
 db.sequelize.sync({ force: false }).then(() => {
   console.log("sync done");
---
+++ model/userModel.js
@@ -0,0 +1,19 @@
+const makeuserTable = (sequelize, DataTypes) => {
+  const user = sequelize.define("user", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return user;
+};
+
+module.exports = makeuserTable;
